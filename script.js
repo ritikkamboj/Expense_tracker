77a6d283c724a2f760ddc3793d03cccec644237c
Aashish Kumar <ritikkamboj4314@gmail.com>
2024-08-23 14:30:37 +0530

    dynamically add the total , income , expense , added the transaction through from and removing the transaction

--- script.js
+++ script.js
@@ -16,7 +16,61 @@ const items = [
 
 let transactions = items;
 console.log(transactions);
+function addTransaction(e)
+{
+  e.preventDefault();
 
+  console.log(text.value);
+  console.log(typeof(amount.value));
+
+  if(text.value.trim() === '' || amount.value === '')
+    alert("You are missing Either of the value to enter");
+  else
+    {
+      let transaction ={
+        id : getId(),
+        text : text.value,
+        amount: +(amount.value)
+      }
+      console.log(typeof(transaction.amount));
+      transactions.push(transaction);
+      init();
+      text.value ='';
+      amount.value =''
+
+    }
+
+
+}
+
+function getId()
+{
+  return Math.random(Math.random()*100000);
+}
+function amountValues(transactions) {
+  // firstly filter the amount in an array and then total that
+  console.log(typeof transactions[0].amount);
+  let total = (transactions
+    .map((item) => item.amount)
+    .reduce((acc, item) => acc + item, 0)).toFixed(2);
+  console.log(total);
+  balance.innerHTML = `$${Math.abs(total)}`;
+
+  // adding the income and expense
+
+  let income = (transactions
+    .filter((item) => item.amount > 0)
+    .reduce((acc, item) => acc + item.amount, 0).toFixed(2));
+    console.log(income)
+  money_plus.innerHTML = `$${income}`;
+
+  let expense = (transactions
+    .filter((item) => item.amount < 0)
+    .reduce((acc, item) => acc + item.amount, 0)).toFixed(2);
+    console.log(expense);
+    
+  money_minus.innerHTML = `$${Math.abs(expense)}`;
+}
 function addElementToDom(transaction) {
   let sign = transaction.amount < 0 ? "-" : "+";
 
@@ -27,15 +81,25 @@ function addElementToDom(transaction) {
 
   item.innerHTML = `${transaction.text}<span>${sign}${Math.abs(
     transaction.amount
-  )}</span><button class="delete-btn">X</button>`;
+  )}</span><button class="delete-btn" onClick="deleteTrans(${transaction.id})">X</button>`;
   // console.log(item);
   list.appendChild(item);
   console.log(list);
 }
 
+function deleteTrans(id){
+  console.log('jai maata di ',id)
+
+  transactions = transactions.filter(item => item.id !== id);
+  init()
+}
+
+form.addEventListener('submit',addTransaction);
+
 function init() {
   list.innerHTML = ``;
 
   transactions.forEach((item) => addElementToDom(item));
+  amountValues(transactions);
 }
 init();
